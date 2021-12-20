7f588fa29b22ed3ccbbb524395f4d04f24fb90e5
sebas1017 <henaoerazosebastian995@gmail.com>
2021-12-20 15:50:32 -0500

    Merge branch 'main' of https://github.com/sebas1017/chatbot_project into main

---
+++ bot_extractor/bot.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+
+@app.route("/bot", methods=["POST"])
+def bot():
+    resp = MessagingResponse()
+    msg = resp.message()
+    word = request.values.get("Body", "").lower()
+    url = f"https://www.amazon.com/-/es/s?k={word}&language=es"
+    headers = {"FUser":"An","user-agent":"an"}
+    response = requests.get(url,headers=headers)
+    if response.status_code == 200:
+        try:
+            soup = BeautifulSoup(response.content,"html.parser")
+            urls = soup.find("div", 
+                            attrs={"class":"s-main-slot s-result-list s-search-results sg-row"}
+                            ).find_all("a",attrs={"class":"a-link-normal a-text-normal"})
+            urls = ["https://www.amazon.com"+ item.get('href') for item in urls[:5] ]
+            response_final = "\n\n URL \n".join(urls)
+            msg.body(response_final)
+            return str(resp)
+        except:
+            msg.body(f"sin resultados para {word}")
+            return str(resp)
+    else:
+        msg.body('Lo sentimos , su busqueda no ha tenido resultados intente con otro articulo')
+        return str(resp)
+    
+
+if __name__ == "__main__":
+    app.run(port=5000)
