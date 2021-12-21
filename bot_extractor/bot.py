c006a4edcfcda40ba96c9b8724abcb9ea2702be3
sebas1017 <henaoerazosebastian995@gmail.com>
2021-12-20 19:49:08 -0500

    cambio en headers amazon

--- bot_extractor/bot.py
+++ bot_extractor/bot.py
@@ -16,8 +16,17 @@ def bot():
     word = request.values.get("Body", "").lower()
     url = f"https://www.amazon.com/-/es/s?k={word}&language=es"
     headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,
-    'referer':'https://www.google.com/'
+    'authority': 'www.amazon.com',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
     response = requests.get(url,headers=headers,timeout=2)
     logging.error(f"RESPONSE_STATUS_CODE {response.status_code}")
